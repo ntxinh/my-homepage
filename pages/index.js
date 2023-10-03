6a14570f7e1c87569a411f0d4a41b99ac6fd951e
ntxinh <nguyentrucxjnh@gmail.com>
2023-10-03 14:59:48 +0700

    :star: filter

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
 
   const [links, setLinks] = useState([]);
   const [quote, setQuote] = useState({ text: '' });
+  const [tabIndex, setTabIndex] = useState(2);
 
   useEffect(() => {
     setLinks(json.links);
@@ -17,25 +18,35 @@ export default function Home() {
     return Math.floor(Math.random() * max);
   }
 
+  const switchTab = (event, currentTabIndex) => {
+    // console.log(event);
+    // console.log(currentTabIndex);
+    setTabIndex(currentTabIndex);
+  }
+
+  const getClassName = (currentTabIndex) => {
+    return `flex justify-center border-t-4 ${tabIndex == currentTabIndex ? 'text-indigo-600 border-indigo-600' : 'border-transparent hover:text-indigo-600 hover:border-indigo-600'} py-4`
+  }
+
   return (
     <div>
       {/* Tabs */}
       {/* https://tailwindtemplates.io/templates */}
       <ul className="grid grid-flow-col text-center text-gray-500">
-        <li>
-          <a href="#page1" className="flex justify-center border-t-4 border-transparent hover:text-indigo-600 hover:border-indigo-600 py-4">Work</a>
+        <li onClick={(event) => switchTab(event, 1)}>
+          <a href="#page1" className={getClassName(1)}>Work</a>
         </li>
-        <li>
-          <a href="#page2" className="flex justify-center border-t-4 text-indigo-600 border-indigo-600 py-4">News</a>
+        <li onClick={(event) => switchTab(event, 2)}>
+          <a href="#page2" className={getClassName(2)}>News</a>
         </li>
-        <li>
-          <a href="#page3" className="flex justify-center border-t-4 border-transparent hover:text-indigo-600 hover:border-indigo-600 py-4">PriceBoard</a>
+        <li onClick={(event) => switchTab(event, 3)}>
+          <a href="#page3" className={getClassName(3)}>PriceBoard</a>
         </li>
-        <li>
-          <a href="#page4" className="flex justify-center border-t-4 border-transparent hover:text-indigo-600 hover:border-indigo-600 py-4">DevOps</a>
+        <li onClick={(event) => switchTab(event, 4)}>
+          <a href="#page4" className={getClassName(4)}>DevOps</a>
         </li>
-        <li>
-          <a href="#page5" className="flex justify-center border-t-4 border-transparent hover:text-indigo-600 hover:border-indigo-600 py-4">Settings</a>
+        <li onClick={(event) => switchTab(event, 5)}>
+          <a href="#page5" className={getClassName(5)}>Settings</a>
         </li>
       </ul>
 
@@ -53,13 +64,13 @@ export default function Home() {
 
         <div className="grid grid-cols-2 md:grid-cols-6 2xl:grid-cols-8 gap-4">
 
-          {links.map((object, i) =>
+          {links.filter(x => x.categoryId == tabIndex).map((object, i) =>
             <a
               key={i}
               href={object.url}
               target="_blank"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-              style={{'background-color': object.color || null}}
+              style={{backgroundColor: object.color || null}}
             >
               {object.name}
             </a>
