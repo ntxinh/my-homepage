edc0a34c28039206a2cad401444540edef2f3d4f
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2023-10-04 16:38:14 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,6 @@ export default function Home() {
             <a
               key={i}
               href={object.url}
-              target="_blank"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
               style={{backgroundColor: object.color || null}}
             >
